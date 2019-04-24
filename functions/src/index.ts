d18f7d8dc1fefac618c9ae72db73644025eec13a
Zeeshan Abid <zabid@waracle.com>
2019-04-24 16:49:31 +0100

    Firebase functions exported

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as admin from 'firebase-admin';
 
+//Functions
+import GetFileList from "./GetFileList";
+import LoadFile from "./LoadFile";
+import SaveFile from "./SaveFile";
+
 admin.initializeApp();
 admin.auth();
 
 export default {
-  
+  GetFileList,
+  SaveFile,
+  LoadFile
 };
